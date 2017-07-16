4dabd62e5253df1f636dcd478307cc6b3e197349
himansudesai <himansudesai@gmail.com>
2017-07-16 23:25:56 +0200

    The backend we have for serving up the ui, can now be brought up in regular mode [ node app.js ] or test mode [ node app.js -test ].  The regular mode will continue to work with real endpoints when the user is manually interacting with the browser.  The test mode can be used for automated bombay.js testing

--- bombayjs-companion-app-backend/app.js
+++ bombayjs-companion-app-backend/app.js
@@ -3,23 +3,22 @@ var request = require('request');
 var app = express();
 
 const PORT_NUM = 4200;
+var stocksUrl = "http://finance.google.com/finance";
+var albumsUrl = "https://itunes.apple.com/search";
+
+if (appStartedInTestMode()) {
+  switchToTesUrls();
+}
 
 // This endpoint is a gateway for calling the itunes service.
 app.use('/albums', function(req, res) {  
-  var url = "https://itunes.apple.com/search" + req.url;
+  var url = albumsUrl + req.url;
   req.pipe(request(url)).pipe(res);
 });
 
 // This endpoint is a gateway for calling the google finance api.
 app.use('/finance', function(req, res) {
-  var url = "http://finance.google.com/finance" + req.url;
-  console.log('++hdd++ req.url = ' + req.url);
-  req.pipe(request(url)).pipe(res);
-});
-
-// This endpoint is a gateway for calling bombayjs endpoints
-app.use('/testfinance', function(req, res) {
-  var url = "http://localhost:3039/finance" + req.url;
+  var url = stocksUrl + req.url;
   req.pipe(request(url)).pipe(res);
 });
 
@@ -28,3 +27,19 @@ app.listen(PORT_NUM, function () {
 });
 
 app.use(express.static("./"));
+
+
+// -- private --
+function appStartedInTestMode() {
+  if (process.argv.length > 2) {
+    const arg3 = process.argv[2];
+    if (arg3.startsWith('-') && (arg3.substring(1)) == 'test' ) {
+      return true;
+    }
+  }
+}
+function switchToTesUrls() {
+  const BOMBAYJS_URL = 'http://localhost:3039';
+  stocksUrl = BOMBAYJS_URL;
+  albumsUrl = BOMBAYJS_URL;
+}
